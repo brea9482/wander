e347f337f04cb3809158a6b45f831cf9b6f7203b
brea9482 <breault.patrick@gmail.com>
2015-07-23 14:34:57 -0400

    devise issues fixed

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,13 +3,15 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
+    sequence(:username) { |n| "username#{n}" }
     password 'password'
     password_confirmation 'password'
   end
 
   factory :playlist do
     sequence(:name) { |n| "name#{n}" }
-    
+    description 'description'
+
     user
   end
 
